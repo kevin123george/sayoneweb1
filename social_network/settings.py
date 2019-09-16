e06c61d90d056e6c337202f7334a9d7ec2f82088
kevin <techatkevin@gmail.com>
2019-09-16 23:58:16 +0530

    make it better

--- social_network/settings.py
+++ social_network/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'social_network.urls'
