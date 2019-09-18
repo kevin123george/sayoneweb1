43d98e2a237acd3d09b7b1829e468a195ecfbd50
kevin <techatkevin@gmail.com>
2019-09-18 12:32:10 +0530

    make it better

--- authentication/views.py
+++ authentication/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
-
+from .forms import UserRegForm
 
 def home(request):
     return render (request,'home.html')
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('board_index')
     else:
-        form = UserCreationForm()
+        form = UserRegForm()
     return render(request, 'registration/signup.html', {
         'form': form
     })
